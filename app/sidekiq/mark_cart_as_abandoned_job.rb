da20f60fbe5690ff066b6c48860885700321b09b
Flavio-LP <flavio5070@outlook.com>
2025-10-27 15:29:38 -0300

    feat: criado a lógica de abando de carrinho para uso no sidek

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -3,5 +3,17 @@ class MarkCartAsAbandonedJob
 
   def perform(*args)
     # TODO Impletemente um Job para gerenciar, marcar como abandonado. E remover carrinhos sem interação. 
+    mark_abandoned_carts
+    remove_old_abandoned_carts
+  end
+
+  def mark_abandoned_carts
+    Cart.where('last_interaction_at <= ? AND (abandoned = ? OR abandoned IS NULL)', 3.hours.ago, false)
+        .update_all(abandoned: true)
+  end
+
+  def remove_old_abandoned_carts
+    Cart.where('abandoned = ? AND last_interaction_at <= ?', true, 7.days.ago)
+        .destroy_all
   end
 end
