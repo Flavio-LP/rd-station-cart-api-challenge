06dfd6035778511f02ce8f21b5e44f5e1f3bde16
Flavio-LP <flavio5070@outlook.com>
2025-10-27 22:19:51 -0300

    fix: corrigido ação de criação do carrinho e busca do produto para inserção, adição e remoção do produto

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController
-  ## TODO Escreva a lógica dos carrinhos aqui
   before_action :set_cart, only: [:show, :add_item, :destroy_item]
+  before_action :set_or_create_cart, only: [:create]
 
   def show
     render json: cart_response
@@ -8,7 +8,7 @@ class CartsController < ApplicationController
 
   def create
     product = Product.find(params[:product_id])
-    cart_item = @cart.cart_items.find_or_initialize_by(product: product)
+    cart_item = @cart.cart_items.find_or_initialize_by(product_id: product.id)
     cart_item.quantity ||= 0
     cart_item.quantity += params[:quantity].to_i
     cart_item.save!
@@ -19,7 +19,7 @@ class CartsController < ApplicationController
 
   def add_item
     product = Product.find(params[:product_id])
-    cart_item = @cart.cart_items.find_or_initialize_by(product: product)
+    cart_item = @cart.cart_items.find_or_initialize_by(product_id: product.id)
     cart_item.quantity ||= 0
     cart_item.quantity += params[:quantity].to_i
     cart_item.save!
@@ -30,7 +30,7 @@ class CartsController < ApplicationController
 
   def destroy_item
     product = Product.find(params[:product_id])
-    cart_item = @cart.cart_items.find_by(product: product)
+    cart_item = @cart.cart_items.find_by(product_id: product.id)
     
     if cart_item
       cart_item.destroy
@@ -42,6 +42,17 @@ class CartsController < ApplicationController
   end
 
   private
+
+  def set_or_create_cart
+    @cart = Cart.find_by(id: session[:cart_id])
+    
+    if @cart.nil?
+      @cart = Cart.create(total_price: 0, last_interaction_at: Time.current, abandoned: false)
+      session[:cart_id] = @cart.id
+    else
+      @cart.update(last_interaction_at: Time.current)
+    end
+  end
 
   def set_cart
     @cart = Cart.find_by(id: session[:cart_id])
