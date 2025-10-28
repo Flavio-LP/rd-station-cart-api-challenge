b4395bb730d3a55faa053779906cb00bd49910ac
Flavio-LP <flavio5070@outlook.com>
2025-10-27 22:20:30 -0300

    fix: corrigido estrutura do modelo

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,19 +1,8 @@
 class CartItem < ApplicationRecord
-
-    has_many :cart_items, dependent: :destroy
-    has_many :products, through: :cart_items
+  belongs_to :cart
+  belongs_to :product
   
-    validates_numericality_of :total_price, greater_than_or_equal_to: 0
+  validates :quantity, numericality: { greater_than: 0 }
+  validates :product_id, uniqueness: { scope: :cart_id }
 
-    def mark_as_abandoned
-        update(abandoned: true)
-    end
-
-    def remove_if_abandoned
-      destroy if abandoned? && last_interaction_at <= 7.days.ago
-    end
-  
-    def abandoned?
-      abandoned == true
-    end
 end
